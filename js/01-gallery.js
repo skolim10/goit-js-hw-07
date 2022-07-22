2273710b5e76ad469234093a0163bf7eaf32685f
Tomasz Skolimo <skolim10@gmail.com>
2022-07-22 14:42:04 +0200

    first_task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const imagesList = document.querySelector(".gallery");
+const images = [];
+
+galleryItems.forEach((img) => {
+  const divItem = document.createElement("div");
+  divItem.classList.add("gallery__item");
+
+  const aItem = document.createElement("a");
+  aItem.href = img.original;
+  aItem.classList.add("gallery__link");
+  aItem.insertAdjacentHTML(
+    "beforeend",
+    `<img class="gallery__image" src=${img.preview} data-source=${img.original} alt=${img.description}>`
+  );
+  divItem.appendChild(aItem);
+
+  images.push(divItem);
+});
+imagesList.append(...images);
+
+imagesList.addEventListener("click", imgClick);
+
+function imgClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.getAttribute(
+      "data-source"
+    )}" width="800" height="600">`);
+
+  instance.show();
+
+  imagesList.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
 
 console.log(galleryItems);
