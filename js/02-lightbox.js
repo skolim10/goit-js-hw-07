04c1279501215f1a0cbc4c85386c3121b41c9367
Tomasz Skolimo <skolim10@gmail.com>
2022-07-22 15:36:10 +0200

    task2_final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const imagesList = document.querySelector(".gallery");
+
+const galleryItem = galleryItems
+  .map(
+    (galleryItem) => `<a class="gallery__item" href="${galleryItem.original}">
+  <img
+    class="gallery__image"
+    src="${galleryItem.preview}"
+    alt="${galleryItem.description}"
+    title="${galleryItem.description}"
+  />
+</a>`
+  )
+  .join("");
+
+imagesList.innerHTML = galleryItem;
+
+let gallery = new SimpleLightbox(".gallery a", { captionDelay: 250 });
+//gallery.on("show.simplelightbox", function () {
+// Do something…
+//});
